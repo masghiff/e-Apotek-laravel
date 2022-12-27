fb7fc544535c81ad0e72bd5692c52f324eec55e4
asidikfauzi <asidikfauzi@gmail.com>
2022-12-27 23:06:30 +0700

    prepare all controller and middleware

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,20 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo;
+
+    protected function redirectTo(){
+
+        if(Auth()->user()->role == "admin")
+        {
+            return route('admin.dashboard');
+        }
+        elseif(Auth()->user()->role == "pelanggan")
+        {
+            return route('pelanggan.dashboard');
+        }
+
+    }
 
     /**
      * Create a new controller instance.
@@ -35,6 +48,36 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+        return redirect()->route('login');
+    }
+
+    public function login(Request $request)
+    {
+        $username = $request->input('username');
+        $password = $request->input('password');
+
+
+        if(auth()->attempt(array('username'=>$username, 'password'=>$password)))
+        {
+
+            if(auth()->user()->role == "admin")
+            {
+                return redirect()->route('admin.dashboard');
+            }
+            elseif(auth()->user()->role == "pelanggan")
+            {
+                return redirect()->route('pelanggan.dashboard');
+            }
+        }
+        else
+        {
+            return redirect()->route('login')->with('failed', 'Username and password are wrong');
+        }
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Auth::routes();
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
